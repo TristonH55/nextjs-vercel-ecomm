3c614601a3865efed5432f34fe721837959ee700
TristonH55 <oop@oopdesign.com.au>
2023-11-23 10:26:01 +1000

    working dynamic page api update

---
+++ pages/[slug].js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+// import {NextUIProvider} from "@nextui-org/react"
+import useSWR from 'swr'
+import { useRouter } from 'next/router'
+
+
+
+
+//working code
+
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+export default function Page() {
+    const router = useRouter();
+    const { query : {slug} } = router
+    console.log(router);
+
+  const { data, error, isLoading } = useSWR(`/api/page/${slug}`, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+
+  return (
+    <div>
+      <Head>
+        <title>Create App</title>
+      </Head>
+      <main>
+        <h1>{data.page.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: data.page.content }} />
+      </main>
+    </div>
+  );
+}
